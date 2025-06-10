4d81a095c4d52c7ce1e45f2a26cbf3734965aff9
Adityax02 <68099257+Adityax02@users.noreply.github.com>
2025-06-10 23:11:37 +0530

    Update TransactionDAO.java

--- src/dao/TransactionDAO.java
+++ src/dao/TransactionDAO.java
@@ -3,9 +3,9 @@ package dao;
 import model.Transaction;
 import util.DBConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransactionDAO {
 
@@ -29,5 +29,51 @@ public class TransactionDAO {
             System.out.println("❌ Failed to add transaction");
             e.printStackTrace();
         }
+    }
+
+    public double getTotalAmountByType(int userId, String type) {
+        String sql = "SELECT SUM(amount) FROM transactions WHERE user_id = ? AND type = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, userId);
+            ps.setString(2, type);
+            var rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getDouble(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public List<Transaction> getAllTransactions(int userId) {
+        List<Transaction> list = new ArrayList<>();
+        String sql = "SELECT * FROM transactions WHERE user_id = ? ORDER BY date DESC";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, userId);
+            var rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Transaction t = new Transaction();
+                t.setTxnId(rs.getInt("txn_id"));
+                t.setUserId(rs.getInt("user_id"));
+                t.setAmount(rs.getDouble("amount"));
+                t.setCategory(rs.getString("category"));
+                t.setType(rs.getString("type"));
+                t.setDate(rs.getDate("date").toLocalDate());
+                t.setNote(rs.getString("note"));
+
+                list.add(t);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
